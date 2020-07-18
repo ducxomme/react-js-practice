97af1dc8b411ce7455e1961fe6e26349eff27e85
Duc Nguyen <ducxomme@gmail.com>
2020-07-18 16:51:47 +0900

    6-component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div className="TodoItem">
+        <p>Go to supermarket</p>
+      </div>
+    )
+  }
+}
+
+export default TodoItem;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <List />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
