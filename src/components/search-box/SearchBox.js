f94354cf0e57900ecca82d11916d2b05769c5472
Duc Nguyen <ducxomme@gmail.com>
2020-07-26 11:28:36 +0900

    12-onChange input phone number 10 character

--- src/components/search-box/SearchBox.js
+++ src/components/search-box/SearchBox.js
@@ -10,12 +10,14 @@ class SearchBox extends Component {
     super();
     this.state = {
       isFocused: false,
-      isExceed: false
+      isExceed: false,
+      isRightNumber: true
     };
 
     this.onFocus = this.onFocus.bind(this);
     this.onBlur = this.onBlur.bind(this);
     this.onChange = this.onChange.bind(this);
+    // this.onInput = this.onInput.bind(this);
   }
 
   onFocus() {
@@ -30,24 +32,46 @@ class SearchBox extends Component {
     });
   }
 
+  // onChange(event) {
+  //   let input = event.target.value;
+  //   if(input.length > 10) {
+  //     this.setState({
+  //       isExceed: true
+  //     })
+  //   } else {
+  //     this.setState({
+  //       isExceed: false
+  //     })
+  //   }
+  // }
+
   onChange(event) {
     let input = event.target.value;
-    if(input.length > 10) {
+    console.log(input);
+    if (!Number(input)) {
       this.setState({
-        isExceed: true
+        isRightNumber: false
+      })
+    } else if (input.length !== 10) {
+      this.setState({
+        isRightNumber: false
+      })
+    } else if (input.slice(0, 3) !== '090') {
+      this.setState({
+        isRightNumber: false
       })
     } else {
       this.setState({
-        isExceed: false
+        isRightNumber: true
       })
     }
   }
 
   render() {
-    const { isFocused, isExceed } = this.state;
+    const { isFocused, isExceed, isRightNumber } = this.state;
     return (
       <div className={classNames('search-box-container', 'mt-5', 'd-flex', 'col-sm-4', 'offset-sm-4', {
-        'red-border': isExceed
+        'red-border': isRightNumber === false
       })}>
         <input
           type="text"
@@ -55,6 +79,8 @@ class SearchBox extends Component {
           onFocus={this.onFocus}
           onBlur={this.onBlur}
           onChange={this.onChange}
+          // onInput={this.onInput}
+          pattern="[0-9]*"
           placeholder="Type something to search..."
 
         />
